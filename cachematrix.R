3a8efab247f11ecad46a21bcd9a703e32ef19f15
vishal.official <vishal.am14@gmail.com>
2018-07-17 10:40:46 +0530

    Coursera_assignment3

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x=matrix()) {
+      ## x is always a square covertible matrix
+      
+      inv = NULL
+      set = function(y){
+            x <<- y
+            inv <<- NULL
+            
+## <<- operator in this case assigns the value of y to x from diff. environment
+## Good article explaining lexical scoping used above
+## https://github.com/lgreski/datasciencectacontent/blob/master/markdown/rprog-breakingDownMakeVector.md
+      }
+       get = function() x
+       ## defines getter for matrix x
+       
+       setinv = function(inverse) {
+             inv <<- inverse
+             ## defines setter for the inverse
+       }
+       getinv = function() inv
+       
+       list(set=set,get=get,setinv=setinv,getinv=getinv)
+      ##Naming the list elements allows us to use the 
+      ##$ form of the extract operator to access the functions by name
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function (x,...) {
+      inv = x$getinv()
+      
+      ##if inverse is already cached
+      if(!is.null(inv)){
+            message ("getting from cache")
+            return(inv)
+      }
+      
+      inv.matrix = x$get()
+      inv = solve(inv.matrix,...)
+      ## calculating the inverse
+      
+      x$setinv()
+      
+      return(inv)
 }
